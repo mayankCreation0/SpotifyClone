618403784ddf7e5e55943ff49651da90a9d5da13
mayank <mayankrj1224@gmail.com>
2023-01-08 15:39:31 +0530

    bugs fixed

--- src/containers/Browse/browseServices.js
+++ src/containers/Browse/browseServices.js
@@ -4,4 +4,4 @@ export const getBrowse = () =>
   api('https://api.spotify.com/v1/browse/categories');
 
 export const getReleases = () =>
-  api('https://api.spotify.com/v1/browse/new-releases?limit=10');
+  api('https://api.spotify.com/v1/browse/new-releases?limit=20');
